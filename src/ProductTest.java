e2fe591b5fedc963b929120f36e7e5c91299d9c0
753813 <753813@123-PC05.student.Tor.Loc>
2019-06-25 17:07:32 -0400

    tests added

--- src/ProductTest.java
+++ src/ProductTest.java
@@ -5,8 +5,17 @@ import org.junit.Test;
 public class ProductTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCreateProduct() {
+		Product p=new Product("chair",40);
+		assertEquals("chair",p.getTitle());
+		assertEquals(40,p.getPrice(),0.1);
+	}
+
+	@Test
+	public void testTwoProductsEqual() {
+		Product p1=new Product("shoes",25);
+		Product p2=new Product("shoes",3025);
+		assertEquals(true,p1.equals(p2));
 	}
 
 }
--- src/ShoppingCartTest.java
+++ src/ShoppingCartTest.java
@@ -5,8 +5,31 @@ import org.junit.Test;
 public class ShoppingCartTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCreateCart() {
+		ShoppingCart s= new ShoppingCart();
+		assertEquals(0,s.getItemCount());
 	}
+	@Test
+	public void testEmptyCart() {
+		ShoppingCart s= new ShoppingCart();
+		Product p = new Product("chair",40);
+		s.addItem(p);
+		s.empty();
+		assertEquals(0,s.getItemCount());
+	}
+	@Test
+	public void testAddProduct() {
+		ShoppingCart s= new ShoppingCart();
+		Product p = new Product("chair",40);
+		double balance=s.getBalance();
+		s.addItem(p);
+		assertEquals(1,s.getItemCount());
+		assertEquals(balance+40,s.getBalance(),0.01);
+	}
+	@Test
+    public void testRemoveProduct()
+    {
+		
+    }
 
 }
